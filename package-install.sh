eec379ee93ebb1f6b07af68474f1b13b657905b2
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-04-15 13:33:46 -0700

    Update package-install.sh

--- package-install.sh
+++ package-install.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-apt-get update
-apt-get upgrade
+# add PPAs
 
-#packages i use
+sudo apt update && upgrade
+
+# packages i use
 PACK="libsox-fmt-mp3 sox cheese gnome-terminal net-tools xinput alsa-utils mupdf rxvt-unicode ffmpeg htop i3lock scrot imagemagick x11-session-utils nautilus ubuntu-drivers-common neovim python-neovim python3-neovim tlp tlp-rdw ranger neofetch steam atom blender gimp spotify-client gir1.2-gtop-2.0 lm-sensors cmake rofi google-chrome-stable python-pip python3-pip gcc powertop"
-LAPPACK="system76-drivers"
+LAPPACK="system76-driver"
+DESKPACK="system76-driver-nvidia"
 
-#install apt packages
-apt-get install -y $APPS4ian
+# install apt packages
+sudo apt install -y $PACK
 
-#install python packages
+# desktop or laptop packages
+read -r -p "Would you like to install packages for (L)aptop, or (D)esktop? [L/D/n] " response
+if [[ "$response" =~ ^([Ll])+$ ]]
+then
+    sudo apt install -y $LAPPACK 
+fi
+if [[ "$response" =~ ^([Dd])+$ ]]
+then
+    sudo apt install -y $DESKPACK
+fi
+
+
+# install python packages
 pip3 install pywal
 
-#install fonts
+# install fonts
 git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ./install.sh
 
-printf 'package install complete'
+echo 'package install complete'
