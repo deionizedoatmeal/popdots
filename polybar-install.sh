7d7537ecb422f8e7a38e020989cada002044c770
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-04-15 14:31:32 -0700

    Update polybar-install.sh

--- polybar-install.sh
+++ polybar-install.sh
@@ -4,8 +4,8 @@
 sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
 
 # clone jaagr's repository
-git clone https://github.com/jaagr/polybar.git
+git clone https://github.com/jaagr/polybar.git polybarrepo
 
-cd polybar && ./build.sh
+cd polybarrepo && ./build.sh
 
 echo 'polybar installed'
