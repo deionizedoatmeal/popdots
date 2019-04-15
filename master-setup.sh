921a37e873b571c18b6c5c252b91c0b73f61082d
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-04-15 13:53:41 -0700

    Create master-setup.sh

---
+++ master-setup.sh
@@ -0,0 +1,33 @@
+!#/bin/bash
+
+# clone the repository with dot files and scripts
+cd
+git clone https://github.com/deionizedoatmeal/setup.git
+
+# install packages
+./setup/package-install.sh
+
+# install airblader's i3gaps
+./setup/i3gaps-install.sh
+
+# install jaagar's polybar
+./setup/polybar-install.sh
+
+# configure i3, polybar, rofi, and zathura
+cp /setup/i3/congfig ~/.config/i3/config
+cp /setup/polybar/config ~/.config/polybar/config
+cp /setup/polybar/launch.sh ~/.config/polybar/launch.sh
+cp /setup/rofi/config ~/.config/rofi/config
+cp /setup/zarthura/zathurarc ~/.config/zathura/zathurarc
+echo 'congifs set up'
+
+# move system scripts into bin
+sudo cp -p /setup/lock.sh /bin/lock
+sudo cp -p /setup/btheadphones_connect.sh /bin/btheadphones_connect
+sudo cp -p /setup/btheadphones_disconnect.sh /bin/btheadphones_disconnect
+echo 'scripts set up'
+
+echo 'master set up complete'
+
+# remove cloned repo
+sudo rm -r ~/setup
