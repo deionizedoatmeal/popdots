51bfcc3bfb719c318a0002a03f7151d341c145f7
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-04-16 14:42:18 -0700

    Update package-uninstall.sh

--- package-uninstall.sh
+++ package-uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKREMOVE='baobab eog evince gedit geary gnome-calculator gnome-calendar gnome-contacts gnome-cotrol-center gnome-font-viewer gnome-screenshot gnome-system monitor gnome-weather seahorse language-selector-gnome totem yelp ghostscript-x gdm3"
+PACKREMOVE='baobab brltty eog evince gedit geary gnome-calculator gnome-calendar gnome-contacts gnome-cotrol-center gnome-font-viewer gnome-screenshot gnome-system monitor gnome-weather seahorse language-selector-gnome totem yelp ghostscript-x gdm3"
 
 sudo apt autoremove -y $PACKREMOVE
 
