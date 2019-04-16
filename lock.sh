eacac4848cc8230a3a08f55b4e390a91d18cb3dd
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-04-15 20:53:55 -0700

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# canibalized from @petvas i3lock-blur https://github.com/petvas/i3lock-blur.git
+# drop this bad boy in your bin, and the lock icon in the i3 config directory, then just call 'exec lock' in your i3 config file, or just use lock as a command
+
 TMPBG=/tmp/screen.png
 LOCK=$HOME/.config/i3/lock.png
 RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
