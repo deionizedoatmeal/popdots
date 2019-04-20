42a69d6250784fcbb25df61e2b2f7ef71560ec6f
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-04-20 15:25:21 -0700

    Update package-uninstall.sh

--- package-uninstall.sh
+++ package-uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKREMOVE='baobab brltty eog evince gedit geary gnome-calculator gnome-calendar gnome-contacts gnome-cotrol-center gnome-font-viewer gnome-screenshot gnome-system monitor gnome-weather seahorse language-selector-gnome totem yelp ghostscript-x'
+PACKREMOVE='brltty eog evince gedit geary gnome-calculator gnome-calendar gnome-contacts seahorse language-selector-gnome totem yelp ghostscripzt-x'
 
 sudo apt autoremove -y $PACKREMOVE
 
