44d41482ac4a1f23486d41f3885911f4f216e98a
Ian K. Bania <48041892+deionizedoatmeal@users.noreply.github.com>
2019-07-15 16:49:09 -0700

    Update and rename permisions.sh to postinstall.sh

--- install-scripts/permisions.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo usermod -a -G dialout ian
-sudo usermod -a -G tty ian
---
+++ install-scripts/postinstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#lm-sensors
+sudo sensors-detect
+
+#swap
+
+#groups
+#sudo usermod -a -G dialout ian
+#sudo usermod -a -G tty ian
