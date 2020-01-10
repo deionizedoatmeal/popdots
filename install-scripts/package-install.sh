40b3b466c39efc4de8f38c1b477739941c31fc85
ian <ian.k.bania@gmail.com>
2020-01-10 11:15:07 -0800

    added google messages to install list

--- install-scripts/package-install.sh
+++ install-scripts/package-install.sh
@@ -3,12 +3,15 @@
 # add PPAs
 sudo add-apt-repository -u ppa:snwh/ppa
 
+# download debs
+wget https://github.com/chrisknepper/android-messages-desktop/releases/download/v3.1.0/android-messages-desktop_3.1.0_amd64.deb
+
 sudo apt update
 sudo apt upgrade
 
 # packages i use
 SYS="exfat-utils golang-go lm-sensors compton libxcb-render0-dev libffi-dev i3lock ffmpeg python-dev python-cffi fonts-fantasque-sans paper-icon-theme rofi mpg123 system76-driver pass imagemagick feh gnome-tweak-tool vlc htop neovim python-neovim python3-neovim tlp tlp-rdw cmake python-pip python3-pip gcc powertop"
-APPS="darktable pinta neofetch steam atom blender gimp spotify-client zathura ranger" 
+APPS="darktable inkscape neofetch steam atom blender gimp spotify-client zathura ranger android-messages-desktop_3.1.0_amd64.deb" 
 LAPTOP=""
 DESKTOP="system76-driver-nvidia"
 
