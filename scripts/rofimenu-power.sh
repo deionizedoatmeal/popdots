2e2fc53afda93a76b276b8194c430a49a5810ff0
ian <ian.k.bania@gmail.com>
2020-02-05 13:20:39 -0800

    typo

--- scripts/rofimenu-power.sh
+++ scripts/rofimenu-power.sh
@@ -9,7 +9,7 @@ battery=" battery"
 # Variable passed to rofi
 options="$preformance\n$balanced\n$battery"
 
-chosen="$(echo -e "$options" | $rofi_command -dmenu -p "profile" -selected-row 2)"
+chosen="$(echo -e "$options" | $rofi_command -dmenu -p "power profile" -selected-row 2)"
 case $chosen in
     $preformance)
         system76-power profile performance
