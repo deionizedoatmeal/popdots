3b8879962da2f8c112ac913779abddbb577d7b5d
ian <ian.k.bania@gmail.com>
2020-02-06 13:22:13 -0800

    update package list

--- install-scripts/package-install.sh
+++ install-scripts/package-install.sh
@@ -10,11 +10,10 @@ sudo apt update
 sudo apt upgrade
 
 # packages i use
-SYS="speedtest-cli open-vpn tty-clock dialog exfat-utils golang-go lm-sensors compton libxcb-render0-dev libffi-dev i3lock ffmpeg python-dev python-cffi fonts-fantasque-sans paper-icon-theme rofi mpg123 system76-driver pass imagemagick feh gnome-tweak-tool vlc htop neovim python-neovim python3-neovim tlp tlp-rdw cmake python-pip python3-pip gcc powertop"
+SYS="libssl-dev speedtest-cli open-vpn tty-clock dialog exfat-utils golang-go lm-sensors compton libxcb-render0-dev libffi-dev i3lock ffmpeg python-dev python-cffi fonts-fantasque-sans paper-icon-theme rofi mpg123 system76-driver pass imagemagick feh gnome-tweak-tool vlc htop neovim python-neovim python3-neovim tlp tlp-rdw cmake python-pip python3-pip gcc powertop"
 APPS="darktable inkscape neofetch steam atom blender gimp spotify-client zathura ranger android-messages-desktop_3.1.0_amd64.deb" 
 LAPTOP=""
 DESKTOP="system76-driver-nvidia"
-
 
 # install apt packages
 sudo apt install -y $SYS
@@ -39,6 +38,12 @@ then
 fi
 echo 'apt packages installed'
 
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install rust packages
+cargo install spotify-tui
+
 # install light utility
 git clone https://github.com/haikarainen/light.git
 cd light
@@ -50,16 +55,16 @@ cd ..
 sudo rm -r light
 echo 'backlight utility installed'
 
+# install go top
+go get github.com/cjbassi/gotop
+sudo mv ~/go/bin/gotop /bin/.
+
 
 # install python packages
 sudo pip3 install pywal
 sudo pip install flashfocus
 sudo pip3 install protonvpn-cli
 echo 'python packages installed'
-
-# install go top
-go get github.com/cjbassi/gotop
-sudo mv ~/go/bin/gotop /bin/.
 
 
 # install fonts WARNING LARGE
