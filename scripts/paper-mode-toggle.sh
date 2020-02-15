78e6036c697e80ae0e2e2b827bf90f2e19fc348b
ian <ian.k.bania@gmail.com>
2020-02-15 14:28:11 -0800

    better paper mode with a script to toggle

---
+++ scripts/paper-mode-toggle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+grepstuff=$(i3-msg -t get_tree | grep -oP 'gaps":{"inner":0,"outer":0,"top":0,"right":.{0,3}' | tail -1 | grep -o '.\{3\}$')
+
+echo $grepstuff
+
+if [ $grepstuff -eq 750 ]; then
+    i3-msg gaps right current set 0;
+    i3-msg gaps left current set 0;
+    i3-msg split h;
+else
+    i3-msg gaps right current set 400;
+    i3-msg gaps left current set 400;
+    i3-msg split v;
+fi
