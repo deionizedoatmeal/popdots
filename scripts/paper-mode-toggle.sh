e93dd027caf2726162436f1b216a2f97f6ee5662
ian <ian.k.bania@gmail.com>
2020-02-18 02:37:29 -0800

    fixed bug wherein paper mode only worked for the first window it was applied to

--- scripts/paper-mode-toggle.sh
+++ scripts/paper-mode-toggle.sh
@@ -5,10 +5,11 @@
 
 #grepstuff=$(i3-msg -t get_tree | grep -oP 'gaps":{"inner":0,"outer":0,"top":0,"right":.{0,3}' | tail -1 | grep -o '.\{3\}$')
 #grepstuff=$()
+#grepstuff=$(i3-msg -t get_tree | grep -oP '750' | tail -1)
 
-grepstuff=$(i3-msg -t get_tree | grep -oP '750' | tail -1)
+grepstuff=$(i3-msg -t get_tree | grep -oP '.{0,250}"focused":true' | grep -oP 'right.{0,5}' | grep -o '.\{3\}$')
 
-#echo $grepstuff
+echo $grepstuff
 
 if [ $grepstuff -eq 750 ]; then
     i3-msg gaps right current set 0;
