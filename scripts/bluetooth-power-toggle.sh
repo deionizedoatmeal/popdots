0d59695f7b16421fa084db20bbf82c7c9703e09c
ian <ian.k.bania@gmail.com>
2020-02-17 17:24:16 -0800

    added rfkill to the bluetooth power toggle

--- scripts/bluetooth-power-toggle.sh
+++ scripts/bluetooth-power-toggle.sh
@@ -1,11 +1,15 @@
-#/bin/bash
+#!/bin/bash
+# by ian k. bania
+# feb '20
 
 status=$(bluetoothctl show | sed -n -e 's/^.*Powered: //p')
 
 if [ "$status" == "no" ]
 then
+    rfkill unblock bluetooth
     bluetoothctl power on
 else
     bluetoothctl power off
+    rfkill block bluetooth
 fi
 
