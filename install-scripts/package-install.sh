14ab2f16555f76eb7348d6137b8ff7756976f9d3
ian <ian.k.bania@gmail.com>
2020-03-09 04:15:03 -0700

    install script adjustments

--- install-scripts/package-install.sh
+++ install-scripts/package-install.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 # change the GDM background 
-sudo cp ../blurrr.jpg /usr/share/backgrounds/pop/kate-hazen-mort1mer.png 
+#sudo cp ../blurrr.jpg /usr/share/backgrounds/pop/kate-hazen-mort1mer.png 
 
 # add PPAs
-sudo add-apt-repository -u ppa:snwh/ppa
-sudo add-apt-repository "deb http://ppa.launchpad.net/samoilov-lex/aftl-stable/ubuntu artful main"
 
 # .deb downloads
 wget https://github.com/chrisknepper/android-messages-desktop/releases/download/v3.1.0/android-messages-desktop_3.1.0_amd64.deb
@@ -15,8 +13,8 @@ sudo apt update
 sudo apt upgrade
 
 # packages i use
-SYS="android-file-transfer macchanger libssl-dev speedtest-cli transmission-cli transmission-daemon open-vpn tty-clock dialog exfat-utils lm-sensors compton libxcb-render0-dev libffi-dev i3lock ffmpeg python-dev python-cffi fonts-fantasque-sans paper-icon-theme rofi mpg123 system76-driver pass imagemagick feh gnome-tweak-tool vlc htop neovim python-neovim python3-neovim tlp tlp-rdw cmake python-pip python3-pip gcc powertop"
-APPS="darktable inkscape neofetch steam atom blender gimp spotify-client zathura ranger android-messages-desktop_3.1.0_amd64.deb" 
+SYS="macchanger libssl-dev speedtest-cli transmission-cli transmission-daemon openvpn tty-clock dialog exfat-utils lm-sensors compton libxcb-render0-dev libffi-dev i3lock ffmpeg python-dev python-cffi fonts-fantasque-sans rofi mpg123 system76-driver pass imagemagick feh gnome-tweak-tool htop neovim python-neovim python3-neovim tlp tlp-rdw cmake python-pip python3-pip gcc powertop"
+APPS="vlc darktable inkscape neofetch steam atom blender gimp spotify-client zathura ranger" 
 LAPTOP=""
 DESKTOP="system76-driver-nvidia"
 
