3d0333d8d3d7dc33e284297dac294cd540d4e7f1
deionizedoatmeal <ian.k.bania@gmail.com>
2020-03-10 19:53:24 -0700

    added anoter dependecy to the polybar installation script

--- install-scripts/polybar-install.sh
+++ install-scripts/polybar-install.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 # install dependencies
-sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
+sudo apt install cmake cmake-data libjsoncpp-dev libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
 
 # clone jaagr's repository
 git clone https://github.com/jaagr/polybar.git polybarrepo
 
-cd polybarrepo && ./build.sh
+cd polybarrepo
+sudo ./build.sh
 
-echo 'polybar-install.sh has completed'
 
-cd ..
-sudo rm -r polybarrepo
