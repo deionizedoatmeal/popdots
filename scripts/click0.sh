4905cb290b646760ddb285afd098382390b3b6d4
deionizedoatmeal <ian.k.bania@gmail.com>
2020-03-18 07:46:19 -0700

    changed sound device volume/brightness increment noise is played on

--- scripts/click0.sh
+++ scripts/click0.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # needs the sound files placed in ~/.config/i3/
-mpg123 ~/.config/i3/Click_Soft_00.mp3
+
+mpg123 -a pulse $HOME/.config/i3/Click_Soft_00.mp3
--- scripts/click1.sh
+++ scripts/click1.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-#stick this in you bin, and place your sound files in to your i3 config directory
-(mpg123 ~/.config/i3/Click_Soft_01.mp3)
+#!/bin/bash
+# needs the sound files placed in ~/.config/i3/
 
+mpg123 -a pulse $HOME/.config/i3/Click_Soft_01.mp3
