af321e475435035bbeace14a2d3f558c52f678c5
deionizedoatmeal <ian.k.bania@gmail.com>
2020-03-27 21:01:23 -0700

    pack list update

--- install-scripts/package-install.sh
+++ install-scripts/package-install.sh
@@ -23,7 +23,7 @@ sudo apt upgrade
 
 # packages i use
 SYS="nvme-cli libxss-dev gnupg2 openssh-client macchanger libssl-dev speedtest-cli transmission-cli transmission-daemon openvpn tty-clock dialog exfat-utils lm-sensors compton libxcb-render0-dev libffi-dev i3lock ffmpeg python-dev python-cffi fonts-fantasque-sans rofi mpg123 system76-driver pass imagemagick feh gnome-tweak-tool htop neovim python-neovim python3-neovim tlp tlp-rdw cmake python-pip python3-pip gcc powertop"
-APPS="vlc gnuplot ncdu torbrowser-launcher darktable inkscape neofetch steam atom blender gimp spotify-client zathura ranger" 
+APPS="vlc gnuplot torbrowser-launcher darktable inkscape neofetch steam atom blender gimp spotify-client zathura ranger" 
 LAPTOP=""
 DESKTOP="busybox busybox-initramfs dropbear-initramfs system76-driver-nvidia openssh-server"
 
