16f14329946120b3c1df16b88a4955c672c205d4
deionizedoatmeal <ian.k.bania@gmail.com>
2020-04-23 21:56:46 -0700

    added a warn message to my backup script

--- scripts/backup-neptune-external.sh
+++ scripts/backup-neptune-external.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo rdiff-backup -v5 --exclude '/home/ian/.*' /home/ian/ /mnt/external/
-
+read -r -p "IS EVERYTHING MOUNTED THAT NEEDS TO BE??? [y/N]" response
+if [[ "$response" =~ ^([Yy])+$ ]]; then
+    sudo rdiff-backup -v5 --exclude '/home/ian/.*' /home/ian/ /mnt/external/
+fi
