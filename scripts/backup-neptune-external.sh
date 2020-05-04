d07da7f859521cd82183801dd7d2389ace88e820
deionizedoatmeal <ian.k.bania@gmail.com>
2020-05-04 00:25:05 -0700

    merging different i3 configs back together

--- scripts/backup-neptune-external.sh
+++ scripts/backup-neptune-external.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo \ 
+lsblk
+echo \ 
 read -r -p "IS EVERYTHING MOUNTED THAT NEEDS TO BE??? [y/N]" response
 if [[ "$response" =~ ^([Yy])+$ ]]; then
     sudo rdiff-backup -v5 --exclude '/home/ian/.*' /home/ian/ /mnt/external/
